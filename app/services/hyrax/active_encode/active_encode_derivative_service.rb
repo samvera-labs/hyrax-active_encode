eaae4c417bad409352400dbecae8b872edca662b
Chris Colvard <cjcolvar@indiana.edu>
2018-09-22 16:38:02 -0400

    Setting for skipping copying derivative to local hyrax derivatives if derivatives will be streamed externally

--- app/services/hyrax/active_encode/active_encode_derivative_service.rb
+++ app/services/hyrax/active_encode/active_encode_derivative_service.rb
@@ -6,15 +6,18 @@ module Hyrax
     class ActiveEncodeDerivativeService < Hyrax::DerivativeService
       attr_accessor :encode_class, :options_service_class
 
-      def initialize(file_set, encode_class: ::ActiveEncode::Base, options_service_class: nil)
+      def initialize(file_set, encode_class: ::ActiveEncode::Base, options_service_class: nil, local_streaming: true)
         super(file_set)
         @encode_class = encode_class
         @options_service_class = options_service_class
+        @local_streaming = local_streaming
       end
 
       def create_derivatives(filename)
         options = options_service_class.call(@file_set) if options_service_class
         options ||= default_outputs(@file_set)
+        options[:file_set_id] = file_set.id
+        options[:derivative_directory] = derivative_directory if local_streaming?
         Hydra::Derivatives::ActiveEncodeDerivatives.create(filename, outputs: options, encode_class: @encode_class)
       end
 
@@ -31,6 +34,10 @@ module Hyrax
       # TODO: Implement this?
       def cleanup_derivatives; end
 
+      def local_streaming?
+        @local_streaming == true
+      end
+
       private
 
         def derivative_directory
@@ -45,10 +52,9 @@ module Hyrax
         def default_outputs(file_set)
           # Defaults adapted from hydra-derivatives (missing webm derivative)
           audio_encoder = Hydra::Derivatives::AudioEncoder.new
-          case file_set
-          when file_set.audio?
+          if file_set.audio?
             [{ outputs: [{ label: 'mp4', ffmpeg_opt: "-s 320x240 -ac 2 -ab 96k -ar 44100 -acodec #{audio_encoder.audio_encoder}" }] }]
-          when file_set.video?
+          elsif file_set.video?
             [{ outputs: [{ label: 'mp4', ffmpeg_opt: "-s 320x240 -g 30 -b:v 345k -ac 2 -ab 96k -ar 44100 -vcodec libx264 -acodec #{audio_encoder.audio_encoder}" }] }]
           else
             []
